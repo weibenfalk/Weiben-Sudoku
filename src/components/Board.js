bb2421c80aa641ec5b52e95af067b77ffa025b08
weibenfalk <thomas.weibenfalk@gmail.com>
2019-05-13 10:12:34 +0200

    Update Board.js

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,33 +1,31 @@
 import React from 'react';
-import Cell from './Cell';
 import PropTypes from 'prop-types';
+import Cell from './Cell';
 
 import { BoardWrapper } from './BoardStyles';
 
-const Board = ({ board, inputCallback }) => {
-	return (
-		<BoardWrapper>
-			<div className="sudokuGrid">
-				{board.map((row, i) =>
-					row.map((cell, j) => (
-						<div
-							key={j}
-							className={`cell ${i % 3 === 2 && i !== 8 ? 'row-divider' : ''} ${j % 3 === 2 && j !== 8
-								? 'col-divider'
-								: ''}`}
-						>
-							<Cell cell={cell} inputCallback={inputCallback} />
-						</div>
-					))
-				)}
-			</div>
-		</BoardWrapper>
-	);
-};
+const Board = ({ board, inputCallback }) => (
+  <BoardWrapper>
+    <div className="sudokuGrid">
+      {board.map((row, i) =>
+        row.map((cell, j) => (
+          <div
+            key={j}
+            className={`cell ${i % 3 === 2 && i !== 8 ? 'row-divider' : ''} ${
+              j % 3 === 2 && j !== 8 ? 'col-divider' : ''
+            }`}
+          >
+            <Cell cell={cell} inputCallback={inputCallback} />
+          </div>
+        ))
+      )}
+    </div>
+  </BoardWrapper>
+);
 
 Board.propTypes = {
-	board: PropTypes.array,
-	inputCallback: PropTypes.func
+  board: PropTypes.array,
+  inputCallback: PropTypes.func,
 };
 
 export default Board;
