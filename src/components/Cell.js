2f9bac15a8ad7b471b10babecbb34e6141162cd4
weibenfalk <thomas.weibenfalk@gmail.com>
2019-05-13 10:11:55 +0200

    Update Cell.js

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,21 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Cell = ({ cell, inputCallback }) => {
-	return (
-		<input
-			type="text"
-			value={cell.value !== 0 ? cell.value : ''}
-			disabled={cell.disabled}
-			className="cell"
-			onChange={e => inputCallback(e.target.value, cell.x, cell.y)}
-		/>
-	);
-};
+const Cell = ({ cell, inputCallback }) => (
+  <input
+    type="text"
+    value={cell.value !== 0 ? cell.value : ''}
+    disabled={cell.disabled}
+    className="cell"
+    onChange={e => inputCallback(e.target.value, cell.x, cell.y)}
+  />
+);
 
 Cell.propTypes = {
-	cell: PropTypes.object,
-	inputCallback: PropTypes.func,
-}
+  cell: PropTypes.object,
+  inputCallback: PropTypes.func,
+};
 
 export default Cell;
